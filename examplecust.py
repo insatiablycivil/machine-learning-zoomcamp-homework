a861cc8a6e8389f0c4603f10260e3eb824894d2f
insatiablycivil <insatiablycivil@gmail.com>
2024-11-18 03:05:58 +0000

    changes

---
+++ examplecust.py
@@ -0,0 +1,28 @@
+## a new customer informations
+customer = {
+  'customerid': '8879-zkjof',
+  'gender': 'female',
+  'seniorcitizen': 0,
+  'partner': 'no',
+  'dependents': 'no',
+  'tenure': 41,
+  'phoneservice': 'yes',
+  'multiplelines': 'no',
+  'internetservice': 'dsl',
+  'onlinesecurity': 'yes',
+  'onlinebackup': 'no',
+  'deviceprotection': 'yes',
+  'techsupport': 'yes',
+  'streamingtv': 'yes',
+  'streamingmovies': 'yes',
+  'contract': 'one_year',
+  'paperlessbilling': 'yes',
+  'paymentmethod': 'bank_transfer_(automatic)',
+  'monthlycharges': 79.85,
+  'totalcharges': 3320.75
+}
+import requests ## to use the POST method we use a library named requests
+url = 'http://localhost:9696/predict' ## this is the route we made for prediction
+response = requests.post(url, json=customer).json() ## post the customer information in json format
+print(response)
+print(result)
